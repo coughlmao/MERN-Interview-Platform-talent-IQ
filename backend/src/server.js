54e12552c81c419682ab3cfafd1f5416c12f496b
Mohit Anand <anandm.mohit@gmail.com>
2025-12-19 10:42:53 +0530

    Add webhooks from Inngest for Clerk & MongoDB for Auth data

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
 
 import ENV from "./lib/env.js";
 import connectDB from "./lib/db.js";
+import { inngest, functions } from './lib/inngest.js'
 
 const app = express();
 const PORT = ENV.PORT || 3000;
 const __dirname = path.resolve();
+
+// middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    // allow cookies to be sent/received in cross-origin requests
+    credentials: true,
+  })
+);
+
+// routes
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({
